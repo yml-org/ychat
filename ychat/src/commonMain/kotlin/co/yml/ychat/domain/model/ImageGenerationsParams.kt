3700e11d16f16db671f73d22a2eabed42d2ad499
Renato <renatoarg@gmail.com>
2023-03-13 10:47:30 -0300

    feat: iOS sample integration

--- ychat/src/commonMain/kotlin/co/yml/ychat/domain/model/ImageGenerationsParams.kt
+++ ychat/src/commonMain/kotlin/co/yml/ychat/domain/model/ImageGenerationsParams.kt
@@ -2,7 +2,7 @@ package co.yml.ychat.domain.model
 
 internal data class ImageGenerationsParams(
     var prompt: String = "",
-    var results: Int = 2,
+    var results: Int = 1,
     var size: String = "256x256",
     var responseFormat: String = "url",
     var user: String = "",
